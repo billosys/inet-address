5ae62373c19acadea78b6753310ea87d2422d4f0
Bjorn Wickstrom <bjorn@billo.systems>
2017-05-27 21:51:41 -0500

    Updated namespaces and added new dep.

--- project.clj
+++ project.clj
@@ -5,7 +5,8 @@
     :name "Eclipse Public License"
     :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [
-    [org.clojure/clojure "1.8.0"]]
+    [org.clojure/clojure "1.8.0"]
+    [potemkin "0.4.3"]]
   :profiles {
     :dev {
       :dependencies [
