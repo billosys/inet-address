2c61632691d4f69ed11fa01db87c67188945c7b9
Duncan McGreggor <oubiwann@gmail.com>
2017-05-27 21:51:41 -0500

    Updated namespaces and added new dep.

--- dev-resources/src/inet/address/dev.clj
+++ dev-resources/src/inet/address/dev.clj
@@ -1,9 +1,9 @@
 (ns inet.address.dev
   (:require
     [clojure.tools.namespace.repl :as repl]
-    [inet.address :as address]
-    [inet.address.four :as address4]
-    [inet.address.six :as address6]
+    [inet.address.core :as address-core]
+    [inet.address.four :as address-four]
+    [inet.address.six :as address-six]
     [trifl.java :refer [show-methods]])
   (:import (java.net InetAddress
                      Inet4Address
