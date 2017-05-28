d7050195b06ce99c2b5ebd5a9cd2a64472d1cd76
Bjorn Wickstrom <bjorn@billo.systems>
2017-05-27 23:45:57 -0500

    Finished the rest of the implementation.
    Also added unit tests.

--- test/inet/address/core_test.clj
+++ test/inet/address/core_test.clj
@@ -1,7 +1,32 @@
 (ns inet.address.core-test
   (:require [clojure.test :refer :all]
-            [inet.address.core :as address]))
+            [inet.address.core :as address-core]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+(defn make-test-host
+  []
+  (address-core/by-address
+    "testhost"
+    (byte-array [1 2 3 4])))
+
+(deftest address->string
+  (is (= "testhost/1.2.3.4" (str (make-test-host)))))
+
+(deftest hash-code
+  (let [address (make-test-host)]
+    (is (= (hash address)
+           (.hashCode address)))))
+
+(deftest local?
+  (address-core/local-link-address? (address-core/localhost)))
+
+(deftest loopback?
+  (address-core/loopback-address? (address-core/loopback)))
+
+(deftest address
+  (is (= [1 2 3 4] (address-core/address (make-test-host)))))
+
+(deftest host-address
+  (is (= "1.2.3.4" (address-core/host-address (make-test-host)))))
+
+(deftest reachable?
+  (is (not (address-core/reachable? (make-test-host) 0))))
